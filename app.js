4cb713fb0bc732051c872036ed13cd971dc5b12d
Alex Close <ajclose@gmail.com>
2017-06-21 15:40:07 -0500

    Create to do list app

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mustache = require('mustache-express')
+const bodyParser = require('body-parser')
+const toDo = ['clean your rum']
+const complete = ['wash the dog']
+
+app.set('view engine', 'mustache')
+app.engine('mustache', mustache())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000, function() {
+  console.log("Working!");
+})
+
+app.get('/', function(request, response){
+  response.render("index", {
+    pageTitle: "To Do List",
+    todos: toDo,
+    complete: complete
+  })
+})
+
+app.post('/', function(request, response) {
+  const task = request.body.task
+  const completed = request.body.complete
+  if (task) {
+    toDo.push(task)
+  } else if (completed) {
+    complete.push(completed)
+    const deleteIndex = toDo.indexOf(completed)
+    toDo.splice(deleteIndex, 1)
+  }
+  response.redirect('/')
+})
