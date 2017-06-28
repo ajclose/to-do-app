88f34d416d767ea682d5fb1da512187c2187028a
Alex Close <ajclose@gmail.com>
2017-06-28 14:59:25 -0500

    Create list using sequelize

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const mustache = require('mustache-express')
 const bodyParser = require('body-parser')
-const toDo = ['clean your room']
-const complete = ['wash the dog']
+// const toDo = ['clean your room']
+// const complete = ['wash the dog']
+const models = require('./models')
 
 
 app.set('view engine', 'mustache')
@@ -16,21 +17,51 @@ app.listen(3000, function() {
 })
 
 app.get('/', function(request, response){
-  response.render("index", {
-    todos: toDo,
-    complete: complete
+  models.Todo.findAll({order: ['createdAt']}).then(function(task) {
+    response.render("index", {
+      todos: task
+      // complete: complete
+    })
   })
+
 })
 
 app.post('/', function(request, response) {
   const task = request.body.task
+  console.log('task', task);
   const completed = request.body.complete
+  console.log('completed', completed);
   if (task) {
-    toDo.push(task)
-  } else if (completed) {
-    complete.push(completed)
-    const deleteIndex = toDo.indexOf(completed)
-    toDo.splice(deleteIndex, 1)
-  }
+    const todo = models.Todo.build({
+      task: task,
+      completed: false
+  });
+  todo.save().then(function (newTodo) {
+})
+  } else if (completed){
+    models.Todo.update({
+      completed: true
+    }, {
+      where: {
+        id: completed
+      }
+
+    }).then(function(todo) {
+    })
+
+    }
+
+  response.redirect('/')
+})
+
+app.post('/delete', function(request, response) {
+  console.log('clicked!');
+  models.Todo.destroy({
+    where: {
+      completed: true
+    }
+  }).then(function(tasks) {
+    console.log(tasks)
+  })
   response.redirect('/')
 })
